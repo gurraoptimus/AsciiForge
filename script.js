1f7b1b5d98883ba5121ae58742d77300044d592b
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-04-29 01:56:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const image1 = new Image();
+image1.src = 'bellebunbunny.jpg'; // Replace this with URL or your image path
+
+class AsciiEffect {
+    #imageCellArray = [];
+    #symbols = [];
+    #pixels = [];
+    #ctx;
+    #width;
+    #height;
+    constructor( ctx, width, height) {
+        this.#ctx = ctx;
+        this.#width = width;
+        this.#height = height;
+        this.#ctx.drawImage(image1, 0, 0, this.#width, this.#height);
+    }
+}
+    // with the path to your image file
+let effect;
+image1.onload = function initalize(){
+    canvas.width = image1.width;
+    canvas.height = image1.height;
+    //ctx.drawImage(image1, 0, 0);
+    effect = new AsciiEffect(ctx, image1.width, image1.height);
+
+}
